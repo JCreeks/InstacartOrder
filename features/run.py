75880f09a0a5f8255366fa7e803c74b1b5ab185a
JCreeks <gj19861103@gmail.com>
2017-07-26 12:12:08 -0400

    Update run.py

--- features/run.py
+++ features/run.py
@@ -16,23 +16,23 @@ sys.path.append(module_path)
 cmd = 'rm ../input/*_dataset.pkl'
 os.system(cmd)
 
-cmd = 'python train_test_preprocess.py'
-os.system(cmd)
+# cmd = 'python train_test_preprocess.py'
+# os.system(cmd)
 
-cmd = 'python perform_category_features.py'
-os.system(cmd)
+# cmd = 'python perform_category_features.py'
+# os.system(cmd)
 
 # cmd = 'python impute_missing_data.py'
 # os.system(cmd)
 
-cmd = 'python generate_decomposition_features.py'
-os.system(cmd)
+# cmd = 'python generate_decomposition_features.py'
+# os.system(cmd)
 
-cmd = 'python perform_feature_discretize.py'
-os.system(cmd)
+# cmd = 'python perform_feature_discretize.py'
+# os.system(cmd)
 
-cmd = 'python generate_feature_distance.py'
-os.system(cmd)
+# cmd = 'python generate_feature_distance.py'
+# os.system(cmd)
 
 # cmd = 'python generate_tsne_features.py'
 # os.system(cmd)
@@ -40,5 +40,5 @@ os.system(cmd)
 cmd = 'python generate_groupby_features.py'
 os.system(cmd)
 
-cmd = 'python perform_other_features.py'
-os.system(cmd)
+# cmd = 'python perform_other_features.py'
+# os.system(cmd)
