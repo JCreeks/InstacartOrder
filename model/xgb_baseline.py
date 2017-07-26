a521e382c5c1a75adba139a65538f324e3d0c87d
JCreeks <gj19861103@gmail.com>
2017-07-26 10:59:04 -0400

    Update xgb_baseline.py

--- model/xgb_baseline.py
+++ model/xgb_baseline.py
@@ -44,8 +44,8 @@ def main():
     }
 
     SEED = 10
-    model = XgbWrapper(seed=SEED, params=xgb_params, cv_train=True, cv_fold=4)
-    model.train(X_train, y_train)
+    model = XgbWrapper(seed=SEED, params=xgb_params, cv_fold=4)
+    model.train(X_train, y_train, cv_train=True)
     
     y_predict = model.predict(X_test)
     X_test.loc[:,'reordered'] = (y_predict > 0.21).astype(int)
