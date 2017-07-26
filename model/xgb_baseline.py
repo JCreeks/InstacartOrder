2e3eccc4ab4b39b3e6a26cfd0ee175be6c967364
JCreeks <gj19861103@gmail.com>
2017-07-26 09:29:03 -0400

    Update xgb_baseline.py

--- model/xgb_baseline.py
+++ model/xgb_baseline.py
@@ -13,27 +13,24 @@ import xgboost as xgb
 from conf.configure import Configure
 from utils import data_util
 
-
+import gc
+import time
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.model_selection import train_test_split
+from model_stack.model_wrapper import XgbWrapper, SklearnWrapper, GridCVWrapper
 
 def main():
     print 'load datas...'
     train, test = data_util.load_dataset()
-
-    y_train_all = train['y']
-    del train['ID']
-    del train['y']
-    id_test = test['ID']
-    del test['ID']
-
     print 'train:', train.shape, ', test:', test.shape
-
-    train_r2_scores = []
-    val_r2_scores = []
-    num_boost_roundses = []
-
+    
+    train.loc[:, 'reordered'] = train.reordered.fillna(0)
+    y_train = train['reordered']
+    X_train = train.drop('reordered', axis=1)
     X_test = test
-    df_columns = train.columns.values
-    dtest = xgb.DMatrix(X_test, feature_names=df_columns)
+    
+    del train, test
 
     xgb_params = {
         'eta': 0.005,
@@ -44,47 +41,20 @@ def main():
         'silent': 1
     }
 
-    for i in range(0, 5):
-        random_state = 42 + i
-        X_train, X_val, y_train, y_val = train_test_split(train, y_train_all, test_size=0.25, random_state=random_state)
-
-        dtrain = xgb.DMatrix(X_train, y_train, feature_names=df_columns)
-        dval = xgb.DMatrix(X_val, y_val, feature_names=df_columns)
-
-        y_mean = np.mean(y_train)
-
-        cv_result = xgb.cv(dict(xgb_params, base_score=y_mean),  # base prediction = mean(target)
-                           dtrain,
-                           num_boost_round=2000,  # increase to have better results (~700)
-                           early_stopping_rounds=50,
-                           )
-
-        num_boost_rounds = len(cv_result)
-        num_boost_roundses.append(num_boost_rounds)
-        model = xgb.train(dict(xgb_params, base_score=y_mean), dtrain, num_boost_round=num_boost_rounds)
-        train_r2_score = r2_score(dtrain.get_label(), model.predict(dtrain))
-        val_r2_score = r2_score(dval.get_label(), model.predict(dval))
-        print 'perform {} cross-validate: train r2 score = {}, validate r2 score = {}'.format(i + 1, train_r2_score,
-                                                                                              val_r2_score)
-        train_r2_scores.append(train_r2_score)
-        val_r2_scores.append(val_r2_score)
-
-    print '\naverage train r2 score = {}, average validate r2 score = {}'.format(
-        sum(train_r2_scores) / len(train_r2_scores),
-        sum(val_r2_scores) / len(val_r2_scores))
-
-    best_num_boost_rounds = sum(num_boost_roundses) // len(num_boost_roundses)
-    print 'best_num_boost_rounds =', best_num_boost_rounds
-    # train model
-    print 'training on total training data...'
-    dtrain_all = xgb.DMatrix(train, y_train_all, feature_names=df_columns)
-    model = xgb.train(dict(xgb_params, base_score=np.mean(y_train_all)), dtrain_all,
-                      num_boost_round=best_num_boost_rounds)
-
-    print 'predict submit...'
-    y_pred = model.predict(dtest)
-    df_sub = pd.DataFrame({'ID': id_test, 'y': y_pred})
-    df_sub.to_csv(Configure.submission_path, index=False)
+    SEED = 10
+    model = XgbWrapper(seed=SEED, params=xgb_params, cv_fold=4)
+    model.train(X_train, y_train)
+    
+    y_predict = model.predict(X_test)
+    X_test.loc[:,'reordered'] = (y_predict > 0.21).astype(int)
+    X_test.loc[:, 'product_id'] = X_test.product_id.astype(str)
+    submit = ka_add_groupby_features_n_vs_1(X_test[X_test.reordered == 1], 
+                                                   group_columns_list=['order_id'],
+                                                   target_columns_list= ['product_id'],
+                                                   methods_list=[lambda x: ' '.join(set(x))], keep_only_stats=True)
+    submit.columns = sample_submission.columns.tolist()
+    submit_final = sample_submission[['order_id']].merge(submit, how='left').fillna('None')
+    submit_final.to_csv("../result/jul25_1.csv", index=False)
 
 
 if __name__ == '__main__':
